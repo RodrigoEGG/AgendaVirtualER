cbadf78b89cf57badb2088ee12d9b586ecfaa01e
RodrigoEGG <92395672+RodrigoEGG@users.noreply.github.com>
2021-11-19 15:12:20 -0600

    Add files via upload
    Se agrego un archivo llamado directorio para guardar los datos ahí

--- ProyectoProgramacion.cpp
+++ ProyectoProgramacion.cpp
@@ -10,19 +10,18 @@ using namespace std;
 int main() 
 {
 
-    string nombre("");
-    string apellido("");
-    string telefono;
-    string correo("");
-    string estado("");
-    string ciudad("");
-    string pais("");
-    int i, ciclo;
+    string nombre, apellido, telefono, correo, estado, ciudad;
+    int i, ciclo, mexico;
     char symbolo_int = 168;
-    ofstream myfile;
+    ofstream myfile, directorio;
+    fstream directorio_buscar;
 
-    cout << symbolo_int <<"Cuantos contactos deseas guardar?";
+    //menu para elegir agregar contacto o consultar datos
+    
+    directorio.open("directorio.txt", fstream::app);
+    cout << symbolo_int << "Cuantos contactos deseas guardar?";
     cin >> ciclo;
+
     for (i = 1; i <= ciclo; i++) //i++ s con paso 1
     {
         cout << "\nIngrese los datos del contacto:\n";
@@ -49,18 +48,55 @@ int main()
         cout << "Correo   = ";
         cin >> correo;
 
-        cout << "Pais     = ";
-        cin >> pais;
-        
+        if (correo.find("@") == std::string::npos)
+        {
+            do
+            {
+                cout << "El correo es invalido. \n";
+                cout << "correo  = ";
+                cin >> correo;
+                correo.find("@");
+            } while (correo.find("@") == std::string::npos);
+        }
+        cout << "Eres de Mexico?(1 = si / 0 = no)    =";
+        cin >> mexico;
+
+        if (mexico == 0)
+        {
+            cout << "Solo se aceptan contactos de mexico";
+            break;
+        }
+
+        if (mexico != 1 & mexico != 0)
+        {
+            do
+            {
+                cout << "Solo responda con 1 o 0\n";
+                cout << "Eres de Mexico? 1 = si / 0 = no    =";
+                cin >> mexico;
+            } while (mexico != 1 & mexico != 0);
+
+            if (mexico == 0)
+            {
+                cout << "Solo se aceptan contactos de mexico";
+                break;
+            }
+        }
+
         cout << "Estado   = ";
         cin >> estado;
 
         cout << "Ciudad   = ";
         cin >> ciudad;
 
-    }
         myfile.open(nombre + apellido + ".txt");
-        myfile << nombre + "\n" + apellido + "\n" + telefono + "\n" + correo + "\n" + estado + "\n" + ciudad + "\n" + pais + "\n";
+        myfile << nombre + "\n" + apellido + "\n" + telefono + "\n" + correo + "\n" + estado + "\n" + ciudad + "\n" + "México" + "\n";
         myfile.close();
+
+        directorio << nombre + " " + apellido + "\n";
+
+        cout<<"Se creo el arichivo "<<nombre+apellido<<".txt";
+    }
+        directorio.close();
         return 0;
 }
